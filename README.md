9ea5b405231bcfd17d4bfb5a755cdfe9734d9f0c
bwan <moris09@naver.com>
2023-07-18 15:17:12 +0900

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# 웹 푸시 클라이언트
+
+## 환경
+
+- 리액트
+
+## 핵심코드
+
+- index.html, mainfest.json, sw.js, script.js
+
+## 참고 사이트
+
+- https://geundung.dev/114
+- https://wonsss.github.io/PWA/web-push-notification/
+- https://www.youtube.com/watch?v=2zHqTjyfIY8
+- https://joshua-dev-story.blogspot.com/2020/11/pwa-installable.html
+- https://web-push-book.gauntface.com/demos/notification-examples/
---
+++ sw.js
@@ -0,0 +1,81 @@
+// 서비스워커 파일은 브라우저, PWA 가 ON 상태일때 동작
+
+// 서비스 워커가 설치되었을 때 이전 버전의 워커가 여전히 대기 상태로 남아있는 것을 방지하고,
+// 최신 버전의 워커가 빠르게 활성화되도록 할 수 있습니다.
+self.addEventListener("install", (event) => {
+	console.log("install");
+	self.skipWaiting();
+});
+
+// 푸시 서비스를 통해 알림이 오면 실행됨
+self.addEventListener("push", (event) => {
+	// if (!(self.Notification && self.Notification.permission === "granted")) {
+	// 	return;
+	// }
+
+	if (Notification.permission !== "granted") {
+		return;
+	}
+
+	const data = event.data?.json() ?? {};
+	console.log("push:", data);
+	const title = data.title || "Web Push";
+	const message = data.body || "Web Push Test";
+	const image = data.image || "img/ai.png";
+	//const icon = data.icon || "img/cat.png";
+
+	const options = {
+		title: title,
+		body: message,
+		image: image,
+		// icon: icon,
+	};
+
+	event.waitUntil(
+		self.registration.showNotification(title, options).then(() => {
+			self.clients.matchAll().then((clients) => {
+				clients.forEach((client) => {
+					client.postMessage({
+						type: "pushEvent", // pushEvent 값을 postMessage()로 전달
+						payload: {
+							title: title,
+							body: options.body,
+							image: options.image,
+							// icon: options.icon,
+						},
+					});
+				});
+			});
+		})
+	);
+});
+
+// 알림을 클릭하는 경우 실행
+self.onnotificationclick = (event) => {
+	console.log("onnotificationclick : ", event.notification.tag);
+	event.notification.close();
+
+	const pushValue = "push";
+
+	event.waitUntil(
+		clients
+			.matchAll({
+				type: "window",
+			})
+			.then((clientList) => {
+				for (const client of clientList) {
+					if (
+						client.url === "https://venerable-cascaron-63d5c2.netlify.app" &&
+						"focus" in client
+					) {
+						client.focus();
+						client.postMessage({ push: pushValue }); // push 값을 postMessage()로 전달
+						return;
+					}
+				}
+				if (clients.openWindow) {
+					clients.openWindow("https://venerable-cascaron-63d5c2.netlify.app");
+				}
+			})
+	);
+};
